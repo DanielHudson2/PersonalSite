44fa870802c1807c12abc65be07c553b73dce32a
Daniel Hudson <35522603+DanielHudson2@users.noreply.github.com>
2023-03-29 22:35:47 +0100

    New: blog post formatting

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const eleventySass = require("eleventy-sass");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventySass);
     eleventyConfig.addPassthroughCopy('src/assets');
+    eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
     return {
         dir: {
             input: "src",
