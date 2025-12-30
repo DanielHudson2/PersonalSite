35dad7e2422cdbd98f94ef97f10c9bbc02edd9ab
DanielHudson2 <danhudson75@gmail.com>
2025-12-30 13:11:44 +0000

    New: modulise JS

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import NavigationToggle from "./components/navigationToggle";
+
+const navigationToggle = new NavigationToggle();
+navigationToggle.init();
---
+++ src/js/components/navigationToggle.js
@@ -0,0 +1,23 @@
+class MenuToggle {
+    constructor() {
+        this.body = document.body;
+        this.navigationToggle = document.querySelector('.navicon');
+        this.navigationActiveBodyClass = 'menu-open';
+        this.navigationActiveToggleClass = 'active';
+    }
+
+    init() {
+        if (!this.navigationToggle) {
+            return;
+        }
+
+        this.navigationToggle.addEventListener('click', () => this.toggleNavigation());
+    }
+
+    toggleNavigation() {
+        this.body.classList.toggle(this.navigationActiveBodyClass);
+        this.navigationToggle.classList.toggle(this.navigationActiveToggleClass);
+    }
+}
+
+export default MenuToggle;
